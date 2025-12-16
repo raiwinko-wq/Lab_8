baf8576278385f331d61a06fcaf4ef63741f21cc
Pond <raiwin_ko@cmu.ac.th>
2025-12-16 19:42:36 +0700

    Lab8_3_FixLeawww

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,65 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+#include<iomanip>
+#include<string>
+using namespace std;
+#include<cmath>
+ 
+int main(){
+    int A;
+    double H;
+    long long B;
+    cout << "Enter your age: ";
+    cin >> A ;
+ 
+    if(A <= 25 ){       
+        cout << "Enter your height: ";
+        cin >> H ; 
+           if(H < 100 ){
+                cout << "Your character = Chopper";
+                
+            }
+            else if (H < 180 && H >= 100){
+                cout << "Your character = Usopp";
+                
+            } 
+
+            else if (H >= 180 ){
+                cout << "Enter your bounty: ";
+                cin >> B; 
+                    if (B > 11 * pow(10,8)){
+                        cout << "Your character = Zoro";
+                        
+                    }
+                        else{
+                        cout << "Your character = Sanji";
+                        
+                    }
+                }
+                }
+    else if ( A <= 60){
+                cout << "Enter your bounty: ";
+                cin >> B ;
+
+                if (B > 5* pow(10,8)){
+                    cout << "Your character = Jinbe";
+                    
+                }
+                else{
+                    cout << "Your character = Franky";
+                    
+                }
+            }
+    else {
+        cout << "Your character = Brook";
+        
+    }
+            
+return 0;
+}
+
+    
+    
+
+
+
+
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,2 +1,60 @@
+#include<iostream>
+#include<iomanip>
+#include<string>
+using namespace std;
+#include<cmath>
+ int main(){
+    int A,H,B;
+    cout << "Enter your age: ";
+    cin >> A ;
+ 
+    if(A <= 25 ){       
+        cout << "\nEnter your height: ";
+        cin >> H ; 
+           if(H < 100 ){
+                cout << "\nYour character = Chopper";
+                return 0;
+            }
+            else if (H < 180 && H >= 100){
+                cout << "\nYour character = Usopp";
+                return 0;
+            } 
 
-    cout << "You have received 2000 c
+            else if (H >= 180 ){
+                cout << "\nEnter your bouty: ";
+                cin >> B ;}
+                    if (B > 11 * pow(10,6)){
+                        cout << "\nYour character = Zoro";
+                        return 0;
+                    }
+                        else{
+                        cout << "\nYour character = Sanji";
+                        return 0;
+                    }
+                    return 0;
+                }
+    else if ( A <= 60){
+                cout << "\nEnter your bouty: ";
+                cin >> B ;
+
+                if (B> 5* pow(10,10)){
+                    cout << "\nYour character = Jinbe";
+                    return 0;
+                }
+                else{
+                    cout << "\nYour character = Franky";
+                    return 0;
+                }
+            }
+    else {
+        cout << "\nYour caharacter = Brook";
+        return 0;
+    }
+            }
+
+    
+    
+
+
+
+
